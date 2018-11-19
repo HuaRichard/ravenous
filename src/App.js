68cc149ae2fd03cd8fc5753dbc5ccbd5aa55d8a6
HuaRichard <huachao0349@gmail.com>
2018-11-19 22:04:05 +1100

    rewrite ravenous with useState

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 import Yelp from './util/Yelp';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      businesses: []
-    }
-    this.searchYelp = this.searchYelp.bind(this);
-  }
-
-  searchYelp(term, location, sortBy) {
+const App = () => {
+  const [businesses, setBusinesses] = useState([]);
+  
+  const searchYelp = (term, location, sortBy) => {
     Yelp.search(term, location, sortBy).then(
-      businesses => this.setState({ businesses: businesses })
+      businesses => setBusinesses(businesses)
     );
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>ravenous</h1>
-        <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={this.state.businesses} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar searchYelp={searchYelp} />
+      <BusinessList businesses={businesses} />
+    </div>
+  );
 }
+
 
 export default App;
